2c7d7e29a7d3ad99014e9740615da93585dc68ad
zhangqi61 <zhangqi61@baidu.com>
2022-10-05 18:32:59 +0800

    【update】

---
+++ buildSrc/src/main/groovy/com/dashingqi/router/gradle/RouterPlugin.groovy
@@ -0,0 +1,15 @@
+
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class RouterPlugin implements Plugin<Project> {
+
+
+    // 实现apply方法，注入插件的逻辑
+    @Override
+    void apply(Project project) {
+
+        println("this is router plugin")
+    }
+}
